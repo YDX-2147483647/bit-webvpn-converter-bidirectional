1e1df8db75333a7f245c69ada89c220fee8a2abd
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-08-10 13:15:37 +0800

    popup

---
+++ src/popup/popup.js
@@ -0,0 +1,40 @@
+import { encrypt_URL, decrypt_URL } from '/common/convert.js'
+
+const open_btn = document.querySelector("#open"),
+    copy_btn = document.querySelector("#copy");
+
+let tab_type, // 'original' 或 'webvpn'
+    target_url;
+
+window.addEventListener("load", async () => {
+    const [tab] = await (chrome.tabs.query({ active: true, currentWindow: true }));
+    try {
+        target_url = decrypt_URL(tab.url);
+        tab_type = "webvpn";
+    } catch (error) {
+        target_url = encrypt_URL(tab.url);
+        tab_type = "original";
+    }
+
+    document.querySelector("#target-type").textContent = tab_type === 'original' ? ' WebVPN' : '校内网';
+})
+
+open_btn.addEventListener('click', () => {
+    chrome.tabs.create({ url: target_url })
+})
+
+copy_btn.addEventListener('click', async () => {
+    copy_btn.classList.remove('success', 'failed');
+
+    const permission_status = await navigator.permissions.query({ name: 'clipboard-write' });
+    if (permission_status.state === 'granted') {
+        try {
+            await navigator.clipboard.writeText(target_url);
+            copy_btn.classList.add('success');
+        } catch (error) {
+            copy_btn.classList.add('failed');
+        }
+    } else {
+        copy_btn.classList.add('failed');
+    }
+})
