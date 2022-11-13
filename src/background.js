d2995e849c583e7028d0d6d1bf234d55f7f2bb90
FrzMtrsprt <alex20020703@163.com>
2022-11-13 23:28:24 +0800

    仅在指定网站显示右键菜单

--- src/background.js
+++ src/background.js
@@ -1,9 +1,11 @@
 import { convert } from './common/convert.js'
 
 chrome.contextMenus.create({
-    "title": '转换并在新标签页打开',
-    "id": 'convert-and-open',
+    title: '转换并在新标签页打开',
+    id: 'convert-and-open',
+    documentUrlPatterns: ['*://*.bit.edu.cn/*']
 });
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId !== 'convert-and-open')
         return;
